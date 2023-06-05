9a4a80bf30c7d81aef50a4db78f492f9b4dfde12
phamhuuminhtam <minhtamk17qth@gmail.com>
2023-06-05 08:48:29 +0700

    update

--- src/component/booking/selectservice.jsx
+++ src/component/booking/selectservice.jsx
@@ -77,19 +77,19 @@ export default function Selectservice() {
 
   return (
     <>
-    <div class="slider-area2">
-                    <div class="slider-height2 d-flex align-items-center">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-xl-12">
-                                    <div class="hero-cap hero-cap2 pt-70 text-center">
-                                    <h2>Chọn dịch vụ</h2>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+      <div class="slider-area2">
+        <div class="slider-height2 d-flex align-items-center">
+          <div class="container">
+            <div class="row">
+              <div class="col-xl-12">
+                <div class="hero-cap hero-cap2 pt-70 text-center">
+                  <h2>Chọn dịch vụ</h2>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <section className="service-area p-3">
         <div className="container">
           <div className="row">
@@ -152,8 +152,12 @@ export default function Selectservice() {
                                   className="flaticon-healthcare-and-medical"
                                   src={item.media[0]?.url}
                                   alt="lỗi"
-                                  style={{ width: "280px",maxWidth:"300px", minHeight: "170px", maxHeight: "200px"}}
-
+                                  style={{
+                                    width: "280px",
+                                    maxWidth: "300px",
+                                    minHeight: "170px",
+                                    maxHeight: "200px",
+                                  }}
                                 ></img>
                               </div>
                               <div className="service-cap">
